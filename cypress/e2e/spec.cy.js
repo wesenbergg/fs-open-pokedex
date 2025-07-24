799bb206521028d0423c0d9a794a85055449666e
Wesenberg <boriss.jerjomkin@novatron.fi>
2025-07-24 21:28:22 +0300

    Add Cypress for end-to-end testing and update package.json scripts and dependencies

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,16 @@
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
+
+  it('single can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('venusaur').click()
+    cy.contains('Next')
+    cy.contains('Previous')
+  })
+})
